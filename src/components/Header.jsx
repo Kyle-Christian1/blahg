1e3e5643beb7a313b822dbf9cc0ecff042fe1f37
Kyle <kylec.thejourney@gmail.com>
2025-01-24 22:56:35 -0700

    fixed for phones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function Header() {
   return (
     <nav id="header-flexbox">
-<<<<<<< HEAD
-=======
       <h3 class="logo">Blahg</h3>
->>>>>>> 74c485a (responsiveness update)
       <a class="nav-link" href="index.ejs">
         Home
       </a>
