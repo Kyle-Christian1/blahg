46b8e4cc47fd09cb85b9c8e8ccbe6d7d88f807ea
Kyle <kylec.thejourney@gmail.com>
2025-01-26 07:03:49 -0700

    css update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 function Header() {
   return (
-    <nav id="header-flexbox">
-      <h3 class="logo">Blahg</h3>
-      <a class="nav-link" href="index.ejs">
-        Home
-      </a>
-      <a class="nav-link" href="/about.html">
-        About
-      </a>
-      <a class="nav-link" href="/contact.html">
-        Contact Us
-      </a>
+    <nav id="header">
+      <div class="logo-box">
+        <h3 class="logo">Blahg</h3>
+      </div>
+      <div class="header-btn-box">
+        <a class="nav-link" href="index.ejs">
+          Home
+        </a>
+        <a class="nav-link" href="/about.html">
+          About
+        </a>
+        <a class="nav-link" href="/contact.html">
+          Contact Us
+        </a>
+      </div>
     </nav>
   );
 }
